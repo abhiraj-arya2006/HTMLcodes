8dda76b79b09b1e1ec869c3f7e40bec2615daff7
abhiraj-arya2006 <69707413+abhiraj-arya2006@users.noreply.github.com>
2020-12-02 20:15:26 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var fname=prompt("What is your first name?");
-var lname=prompt("What is your last name?");
-var age=prompt("What is your age?");
-var state=prompt("In which STATE/UT do you live?")
-var name=fname + " " + lname;
-
-console.log("Your full name is " + name);
-console.log("You are " + age +" years old");
-console.log("You live in " + state)
